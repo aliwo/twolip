b565e2cde96a3ff1c63470986777314b7faa8655
SeungWon Jeong <aliwo@naver.com>
2020-07-30 18:26:19 +0900

    connection string을 환경변수에서 가져오도록

--- libs/database/engine.py
+++ libs/database/engine.py
@@ -1,10 +1,11 @@
+import os
 from datetime import datetime
 from flask import g
 from sqlalchemy import create_engine, types, Integer, Column
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('mysql+pymysql://root:22380476@localhost/twolip', pool_recycle=300)
+engine = create_engine(os.environ.get('TWOLIP_DATABASE_URL'), pool_recycle=300)
 SessionMaker = sessionmaker(bind=engine)
 
 
